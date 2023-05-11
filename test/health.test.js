83e82a2af4eb745a28d06e4684cdf63d0c853148
tsorak <karost0249@gmail.com>
2023-05-11 15:12:55 +0200

    Add tests with jest, supertest

---
+++ test/health.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+
+const app = require("../src/server.js");
+
+describe("GET /", () => {
+  test("/ returns 200", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toBe("application/json");
+    expect(response.body).toStrictEqual({ message: "Hello, world!" });
+  });
+});
