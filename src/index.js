51706ef85e7abca14d4638ada2235b783f72b679
tsorak <karost0249@gmail.com>
2023-05-11 15:43:59 +0200

    Add Dockerfile

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require("./server.js");
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
 });
