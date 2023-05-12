b5cbb54c4a78f1ec30d0004d59fe5d42dcdf71ca
tsorak <karost0249@gmail.com>
2023-05-12 14:42:14 +0200

    Add sum endpoint

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const sumController = require("./controller/sum.js");
+
 const router = express.Router();
 
 router.get("/", (_req, res) => {
   res.json({ message: "Hello, world!" });
 });
+
+router.get("/sum", sumController);
 
 module.exports = router;
